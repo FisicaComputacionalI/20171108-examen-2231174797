ee58b8b6f43affce418eeb36da17c93ad793a30f
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-11-10 14:13:12 -0600

    Add files via upload
    Ya había subido el programa pero yo introducía el valor de x, pero noté que no se hacía así, por lo que el diagrama de flujo no se cumplía, ahora subó nuevamente el archivo pero es .cpp ya que lo hice en una plataforma en línea porque nos sacaron del salón.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=1;
+    if(x<=5)
+    {
+        while(x<=5)
+    {
+        x=x+2;
+    }
+    if(x%3==0)
+    {
+        x=x+2;
+    }
+    else
+    {
+        cout<<"el numero es_"<<x<<endl;
+    }
+}
+if(x>5)
+{
+    
+}
+return 0;
+}
+
+
